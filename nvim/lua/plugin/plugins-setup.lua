38bedd2eb5e933b60ff0cffb5a7b4411434f0e91
Romankrasawa <romankrasawa8567@gmail.com>
2022-12-28 13:32:46 +0200

    change install script

--- nvim/lua/plugin/plugins-setup.lua
+++ nvim/lua/plugin/plugins-setup.lua
@@ -2,12 +2,12 @@ local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    print("not good")
+ 
     fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     vim.cmd [[packadd packer.nvim]]
     return true
   end
-  print("norm")
+
   return false
 end
 
